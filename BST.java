a5c9c9cc42cb425fe9878fe5dda6550e4c211446
Güvenç Usanmaz <gusanmaz@gmail.com>
2022-05-10 02:37:48 +0300

    Create BST.java

---
+++ BST.java
@@ -0,0 +1,80 @@
+public class BST {
+    TreeNode root;
+
+    public BST(){
+        root = null;
+    }
+
+    public BST(TreeNode node){
+        root = node;
+    }
+
+
+    public TreeNode Add(int val){
+        TreeNode newNode = new TreeNode(val);
+        if (root == null){
+            root = newNode;
+            return root;
+        }
+
+        TreeNode p = null;
+        TreeNode n = root;
+
+        while (n != null){
+            if (n.val < val){
+                p = n;
+                n = n.right;
+            }
+            else{
+                p = n;
+                n = n.left;
+            }
+        }
+
+        if (p.val < val){
+            p.right = newNode;
+        }
+        else{
+            p.left = newNode;
+        }
+        return root;
+    }
+
+    public void PrintInOrder(){
+        InOrder(root);
+    }
+
+    public void InOrder(TreeNode node){
+        if (node == null){
+            return;
+        }
+        InOrder(node.left);
+        System.out.println(node.val);
+        InOrder(node.right);
+    }
+
+    // Bu metod ikili arama agacinin elemanalarini preorder sirada ekrana yazdirir.
+    // Bu metodu yazabilmek icin sinif icinde yardimci bir metod tanimi yapabilirsiniz.
+    // Ornegin PrintInOder metodunun yazimi icin InOrder yardimci metodu yazilmistir.
+    public void PrintPreOrder(){
+        // Metodun icini uygun sekilde doldurunuz.
+    }
+
+
+    // Bu metod ikili arama agacinin elemanalarini postorder sirada ekrana yazdirir.
+    // Bu metodu yazabilmek icin sinif icinde yardimci bir metod tanimi yapabilirsiniz.
+    // Ornegin PrintInOder metodunun yazimi icin InOrder yardimci metodu yazilmistir.
+    public void PrintPostOrder(){
+        // Metodun icini uygun sekilde doldurunuz.
+    }
+
+    
+    // Bu metod bu objenin ikili arama agaci ozelligi tasiyip tasimadigini kontrol eder.
+    // Bu objedenin temsil ettigi ikili agac; ikili arama agaci ise bu metod true
+    // aksi durumda false degerinin dondurur.
+    // Bu metodu yazabilmek icin sinif icinde yardimci bir metod tanimi yapabilirsiniz.
+    // Ornegin PrintInOder metodunun yazimi icin InOrder yardimci metodu yazilmistir.
+    public boolean IsBST(){
+        // Metodun icini uygun sekilde doldurunuz.
+    }
+}
